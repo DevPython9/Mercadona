a34142071509cf5bece1851707aa249c8b410ce5
Abou Dia <python.magic.dia@gmail.com>
2023-10-22 06:19:46 +0200

    insertion données dans le template via le view

--- src/mercadonaProject/views.py
+++ src/mercadonaProject/views.py
@@ -1,8 +1,15 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
-
+from datetime import datetime
 # def index(request):
 #     return HttpResponse("<h1>Bienvenue sur Mercado Store </h1>")
 
+# def index(request):
+#     return render(request, "index.html")
+
+# Insérer des données dans notres templates via des variables
 def index(request):
-    return render(request, "index.html")
+    date = datetime.today()
+    return render(request, "index.html", context={"prenom":"Patrick","date_du_Jour": date})
+# On pouvait insérer directement datetime.today() dans le context sans créer de variable date.
+# On peut aller
