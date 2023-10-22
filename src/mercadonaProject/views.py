c26e9691a8ad83924e36f47dea734764ea4c66e8
Abou Dia <python.magic.dia@gmail.com>
2023-10-22 04:50:05 +0200

    ajout de index.html dans le dossier templates créés dans le projet avt création appli. Modification du views pour mettre render à la place de HttpResponse

--- src/mercadonaProject/views.py
+++ src/mercadonaProject/views.py
@@ -1,4 +1,8 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from django.shortcuts import render
+
+# def index(request):
+#     return HttpResponse("<h1>Bienvenue sur Mercado Store </h1>")
 
 def index(request):
-    return HttpResponse("<h1>Bienvenue sur Mercado Store </h1>")
+    return render(request, "index.html")
